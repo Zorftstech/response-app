1a576b3627eaa677a6cc11935e377d4c7a3a5599
Iysah <yusufiysah@gmail>
2023-08-24 18:05:10 +0100

    project setup

---
+++ src/components/navbar/navbar.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Navbar() {
+    return (
+        <>
+            <h3>Navbar</h3>
+        </>
+    )
+}
---
+++ src/styles/theme.js
@@ -0,0 +1,16 @@
+export const sizes = {
+    wrapperWidth: '1128px'
+}
+export const breakpoints = {
+    lg: '@media screen and (max-width: 1000px)',
+    md: '@media screen and (max-width: 700px)',
+    sm: '@media screen and (max-width: 500px)',
+    xs: '@media screen and (max-width: 250px)',
+}
+
+export const colors = {
+    primaryColor: '#185BB5',
+    textColor: '#555',
+    darkColor: '#000',
+    bgColor: '#FFF9E9',
+}
