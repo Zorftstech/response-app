e678b3d92aa107ee192ed7c43a2bd9a8b704a719
Iysah <yusufiysah@gmail>
2023-08-27 14:37:37 +0100

    updated netlify form

--- src/components/newsletter/index.js
+++ src/components/newsletter/index.js
@@ -10,7 +10,8 @@ export default function Newsletter() {
         <MainWrapper id="waitlist" >
             <h3>Stay Informed and Stay Ahead: Subscribe<br /> to Our App for Exclusive Automotive Tips,<br /> Updates, and Special Offers</h3>
 
-            <form name="contact" method="POST" className="newsletter-form" data-netlify='true'>
+            <form name="contact" method="POST" data-netlify-honeypot="bot-field" className="newsletter-form" data-netlify='true'>
+                <input type="hidden" name="form-name" value="contact" />
                 <input type='email' name='email' placeholder="Enter your email address" />
                 <SubmitButton style={{ backgroundColor: '#fff', color: '#185BB5' }} type='submit' >Join Our Waitlist</SubmitButton>
             </form>
